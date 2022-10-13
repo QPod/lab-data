adff184c22fb2c91f0f5ac09a086302c4daaba1b
QPod0 <QPod0@noreply@github.com>
2022-10-14 03:14:10 +0800

    add home

--- docker_kafka/work/script-confluent-kafka.sh
+++ docker_kafka/work/script-confluent-kafka.sh
@@ -5,14 +5,17 @@ setup_confluent_kafka() {
   local VER_C_KAFKA_MINOR=${VERSION_CONFLUENT_KAFKA:-"7.2.2"}
   local VER_C_KAFKA_MAJOR=${VERSION_CONFLUENT_KAFKA:-"7.2"}
   install_tar_gz "http://packages.confluent.io/archive/${VER_C_KAFKA_MAJOR}/confluent-${VER_C_KAFKA_MINOR}.tar.gz"
-  ls -alh /opt/*
+  mv /opt/confluent-* /opt/kafka
+  export KAFKA_HOME=/opt/kafka
+  ls -alh ${KAFKA_HOME}/*
+}
 
+setup_confluent_kafka_kraft() {
   # ref: https://github.com/confluentinc/cp-all-in-one/blob/7.2.1-post/cp-all-in-one-kraft/update_run.sh
-  # export KAFKA_HOME=/opt/kafka/
   # Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
-  # sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' ${KAFKA_HOME}/etc/confluent/docker/configure
+  sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' ${KAFKA_HOME}/etc/confluent/docker/configure
   # Docker workaround: Ignore cub zk-ready
-  # sed -i 's/cub zk-ready/echo ignore zk-ready/' ${KAFKA_HOME}/etc/confluent/docker/ensure
+  sed -i 's/cub zk-ready/echo ignore zk-ready/' ${KAFKA_HOME}/etc/confluent/docker/ensure
   # KRaft required step: Format the storage directory with a new cluster ID
-  # echo "kafka-storage format --ignore-formatted -t $(kafka-storage random-uuid) -c ${KAFKA_HOME}/etc/kafka/kafka.properties" >> ${KAFKA_HOME}/etc/confluent/docker/ensure
+  echo "kafka-storage format --ignore-formatted -t $(kafka-storage random-uuid) -c ${KAFKA_HOME}/etc/kafka/kafka.properties" >>${KAFKA_HOME}/etc/confluent/docker/ensure
 }
