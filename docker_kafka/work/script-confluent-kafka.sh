2f09a5cde5c33d7c7d4a1b31b19fede5ce0a7ae4
Bibo Hao <haobibo@users.noreply.github.com>
2023-04-12 18:15:18 +0800

    update CKafka pkgs

--- docker_kafka/work/script-confluent-kafka.sh
+++ docker_kafka/work/script-confluent-kafka.sh
@@ -2,16 +2,18 @@ source /opt/utils/script-utils.sh
 
 setup_confluent_kafka() {
   # ref: https://docs.confluent.io/platform/current/installation/installing_cp/zip-tar.html#get-the-software
-  local VER_C_KAFKA_MINOR=${VERSION_CONFLUENT_KAFKA:-"7.2.2"}
-  local VER_C_KAFKA_MAJOR=${VERSION_CONFLUENT_KAFKA:-"7.2"}
-  install_tar_gz "http://packages.confluent.io/archive/${VER_C_KAFKA_MAJOR}/confluent-${VER_C_KAFKA_MINOR}.tar.gz"
+  local VER_C_KAFKA_MINOR=${KAFKA_VERSION:-"7.3.3"}
+  local VER_C_KAFKA_MAJOR=${VER_C_KAFKA_MINOR%.*}
+  local URL_C_KAFKA="http://packages.confluent.io/archive/${VER_C_KAFKA_MAJOR}/confluent-${VER_C_KAFKA_MINOR}.tar.gz"
+  echo "Installing Confluent Kafka from: $URL_C_KAFKA}"
+  install_tar_gz "${URL_C_KAFKA}"
   mv /opt/confluent-* /opt/kafka
   export KAFKA_HOME=/opt/kafka
   ls -alh ${KAFKA_HOME}/*
 }
 
 setup_confluent_kafka_kraft() {
-  # ref: https://github.com/confluentinc/cp-all-in-one/blob/7.2.1-post/cp-all-in-one-kraft/update_run.sh
+  # ref: https://github.com/confluentinc/cp-all-in-one/blob/7.3.3-post/cp-all-in-one-kraft/update_run.sh
   # Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
   sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' ${KAFKA_HOME}/etc/confluent/docker/configure
   # Docker workaround: Ignore cub zk-ready
