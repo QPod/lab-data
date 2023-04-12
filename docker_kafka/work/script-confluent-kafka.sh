8ccb738deecca9183c0d28a9e4d7cb39c5cd90be
Bibo Hao <haobibo@users.noreply.github.com>
2023-04-12 19:15:10 +0800

    bugfix for docker

--- docker_kafka/work/script-confluent-kafka.sh
+++ docker_kafka/work/script-confluent-kafka.sh
@@ -7,17 +7,22 @@ setup_confluent_kafka() {
   local URL_C_KAFKA="http://packages.confluent.io/archive/${VER_C_KAFKA_MAJOR}/confluent-${VER_C_KAFKA_MINOR}.tar.gz"
   echo "Installing Confluent Kafka from: $URL_C_KAFKA}"
   install_tar_gz "${URL_C_KAFKA}"
-  mv /opt/confluent-* /opt/kafka
+  install_zip https://github.com/confluentinc/kafka-images/archive/refs/heads/master.zip
+
   export KAFKA_HOME=/opt/kafka
+  mv /opt/confluent-* /opt/kafka
+  mv /opt/kafka-images* ${KAFKA_HOME}/docker
+  mv "${KAFKA_HOME}/docker/kafka/include/etc/confluent/docker/"  "${KAFKA_HOME}/etc/"
+  ln -sf ${KAFKA_HOME}/etc /etc/confluent
   ls -alh ${KAFKA_HOME}/*
 }
 
 setup_confluent_kafka_kraft() {
   # ref: https://github.com/confluentinc/cp-all-in-one/blob/7.3.3-post/cp-all-in-one-kraft/update_run.sh
   # Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
-  sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' ${KAFKA_HOME}/etc/confluent/docker/configure
+  sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
   # Docker workaround: Ignore cub zk-ready
-  sed -i 's/cub zk-ready/echo ignore zk-ready/' ${KAFKA_HOME}/etc/confluent/docker/ensure
+  sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
   # KRaft required step: Format the storage directory with a new cluster ID
-  echo "kafka-storage format --ignore-formatted -t $(kafka-storage random-uuid) -c ${KAFKA_HOME}/etc/kafka/kafka.properties" >>${KAFKA_HOME}/etc/confluent/docker/ensure
+  echo "kafka-storage format --ignore-formatted -t $(kafka-storage random-uuid) -c /etc/kafka/kafka.properties" >> /etc/confluent/docker/ensure
 }
