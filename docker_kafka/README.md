1e57fe04cb757d540c6ae037f42357a88406b604
Bibo Hao <haobibo@users.noreply.github.com>
2023-04-13 11:34:16 +0000

    debug kraft

---
+++ docker_kafka/README.md
@@ -0,0 +1,28 @@
+## debug
+
+```bash
+docker run -it  --name=tmp-ckafka  -v $(pwd):/tmp/dev -h=broker  qpod/jdk11 bash
+
+export KAFKA_BROKER_ID=1
+export KAFKA_LISTENER_SECURITY_PROTOCOL_MAP='CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT'
+export KAFKA_ADVERTISED_LISTENERS='PLAINTEXT://broker:29092,PLAINTEXT_HOST://localhost:9092'
+export KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1
+export KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS=0
+export KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1
+export KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1
+export KAFKA_JMX_PORT=9101
+export KAFKA_JMX_HOSTNAME=localhost
+export KAFKA_PROCESS_ROLES='broker,controller'
+export KAFKA_NODE_ID=1
+export KAFKA_CONTROLLER_QUORUM_VOTERS='1@broker:29093'
+export KAFKA_LISTENERS='PLAINTEXT://broker:29092,CONTROLLER://broker:29093,PLAINTEXT_HOST://0.0.0.0:9092'
+export KAFKA_INTER_BROKER_LISTENER_NAME='PLAINTEXT'
+export KAFKA_CONTROLLER_LISTENER_NAMES='CONTROLLER'
+export KAFKA_LOG_DIRS='/tmp/kraft-combined-logs'
+
+export COMPONENT=kafka
+export CLUSTER_ID="${CLUSTER_ID:-$(kafka-storage random-uuid)}"
+/etc/confluent/docker/run > /tmp/kafka.log
+```
+
+ERROR [ClusterLinkMetadataManager-broker-1] Cluster link metadata topic creation failed: org.apache.kafka.common.errors.InvalidReplicationFactorException: Unable to replicate the partition 3 time(s): The target replication factor of 3 cannot be reached because only 1 broker(s) are registered. (kafka.server.linkClusterLinkMetadataManagerWithKRaftSupport)
--- docker_kafka/work/script-confluent-kafka.sh
+++ docker_kafka/work/script-confluent-kafka.sh
@@ -1,30 +1,46 @@
 source /opt/utils/script-utils.sh
 
 setup_confluent_kafka() {
-  # ref: https://docs.confluent.io/platform/current/installation/installing_cp/zip-tar.html#get-the-software
+  export KAFKA_HOME=/opt/kafka
+
   local VER_C_KAFKA_MINOR=${KAFKA_VERSION:-"7.3.3"}
   local VER_C_KAFKA_MAJOR=${VER_C_KAFKA_MINOR%.*}
   local URL_C_KAFKA="http://packages.confluent.io/archive/${VER_C_KAFKA_MAJOR}/confluent-${VER_C_KAFKA_MINOR}.tar.gz"
-  echo "Installing Confluent Kafka from: $URL_C_KAFKA}"
-  install_tar_gz "${URL_C_KAFKA}"
+
+  # Downlaod CKafka package and unzip to /opt/kafka
+  # ref: https://docs.confluent.io/platform/current/installation/installing_cp/zip-tar.html#get-the-software
+  install_tar_gz "${URL_C_KAFKA}" && mv /opt/confluent-* ${KAFKA_HOME} \
+ && echo "Setting up kafka dirs:" && mkdir -pv /var/lib/kafka/data /etc/kafka/secrets \
+ && ln -sf ${KAFKA_HOME}/etc /etc/confluent \
+ && ls -alh ${KAFKA_HOME}/*
+
+  # CKafka docker images requires confluent docker utils for dub/cub command
+  pip install -U confluent-kafka https://github.com/confluentinc/confluent-docker-utils/archive/refs/heads/master.zip \
+ && install_zip https://github.com/confluentinc/confluent-docker-utils/archive/refs/heads/master.zip \
+ && PYTHON_SITE=$(python3 -c 'import sys;print(list(filter(lambda s: "site" in s, sys.path))[0])') \
+ && cp -rf /opt/confluent-*/confluent ${PYTHON_SITE} \
+ && rm -rf /opt/confluent-*
+
+  install_zip https://github.com/confluentinc/common-docker/archive/refs/heads/master.zip \
+ && mv /opt/common-docker-master ${KAFKA_HOME}/common-docker \
+ && mkdir -pv ${KAFKA_HOME}/etc/docker/ \
+ && cp -rf ${KAFKA_HOME}/common-docker/base/include/etc/confluent/docker/* ${KAFKA_HOME}/etc/docker/
+
+  # CKafka base docker images are built with some scripts included
   install_zip https://github.com/confluentinc/kafka-images/archive/refs/heads/master.zip
-
-  export KAFKA_HOME=/opt/kafka
-  mv /opt/confluent-* /opt/kafka
-  mv /opt/kafka-images* ${KAFKA_HOME}/docker
-  mv "${KAFKA_HOME}/docker/kafka/include/etc/confluent/docker/"  "${KAFKA_HOME}/etc/"
-  ln -sf ${KAFKA_HOME}/etc /etc/confluent
-  ls -alh ${KAFKA_HOME}/*
-
-  echo "Setting up kafka dirs:" && mkdir -pv /var/lib/kafka/data /etc/kafka/secrets
+  mv /opt/kafka-images* ${KAFKA_HOME}/kafka-images
+  cp -rf "${KAFKA_HOME}/kafka-images/kafka/include/etc/confluent/docker/"  "${KAFKA_HOME}/etc/"
 }
 
 setup_confluent_kafka_kraft() {
   # ref: https://github.com/confluentinc/cp-all-in-one/blob/7.3.3-post/cp-all-in-one-kraft/update_run.sh
-  # Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
+  # Docker workaround 1: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
+  # Docker workaround 2: Ignore cub zk-ready
   sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
-  # Docker workaround: Ignore cub zk-ready
   sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
+
   # KRaft required step: Format the storage directory with a new cluster ID
-  echo "kafka-storage format --ignore-formatted -t $(kafka-storage random-uuid) -c /etc/kafka/kafka.properties" >> /etc/confluent/docker/ensure
+cat >>/etc/confluent/docker/configure <<EOF
+kafka-storage format --ignore-formatted -t "\${CLUSTER_ID}" -c /etc/kafka/kafka.properties
+EOF
 }
