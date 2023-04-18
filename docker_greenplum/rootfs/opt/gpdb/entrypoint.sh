5e41ef6e1dab5c50be27623eaf69f7ffd3912d9d
Bibo Hao <haobibo@users.noreply.github.com>
2023-04-18 08:30:52 +0000

    bugfix

--- docker_greenplum/rootfs/opt/gpdb/entrypoint.sh
+++ docker_greenplum/rootfs/opt/gpdb/entrypoint.sh
@@ -3,14 +3,13 @@
 /var/run/sshd/sshd_start.sh && sleep 5s
 
 if [ "`ls -A $GPDB_DATA`" = "" ]; then
-    mkdir -pv $GPDB_DATA/master $GPDB_DATA/primary
-    export MASTER_DATA_DIRECTORY=$GPDB_DATA/master/gpseg-1/
+    export COORDINATOR_DIRECTORY=$GPDB_DATA/coordinator
 
     gpinitsystem -a --ignore-warnings -c /home/gpadmin/gpinitsystem_config_singlenode -h /home/gpadmin/gp_hosts_list
     psql -d postgres -U gpadmin -f /home/gpadmin/initdb_gpdb.sql
     gpconfig -c log_statement -v none
     gpconfig -c gp_enable_global_deadlock_detector -v on
-    echo \"host  all  all  0.0.0.0/0  password\" >> $GPDB_DATA/master/gpseg-1/pg_hba.conf"
+    echo 'host  all  all  0.0.0.0/0  password' >> ${COORDINATOR_DIRECTORY}/gpseg-1/pg_hba.conf
     sleep 5s && gpstop -u && tail -f gpAdminLogs/*.log
 else
     gpstart -a && tail -f gpAdminLogs/*.log
