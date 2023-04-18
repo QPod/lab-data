d018347ae9a64b2f91942fa89c1e974db7bc889d
Bibo Hao <haobibo@users.noreply.github.com>
2023-04-18 19:47:58 +0000

    fix

--- docker_greenplum/README.md
+++ docker_greenplum/README.md
@@ -3,7 +3,9 @@ docker build -t qpod0dev/greenplum --build-arg "BASE_NAMESPACE=qpod" .
 
 docker run -it \
   -p 15432:5432  -p 10022:22 \
-  -v $(pwd):/opt/dev \
+  -v $(pwd):/data/gpdb \
+  -h gpdb-cdw \
+  --name gpdb-cdw \
   docker.io/qpod0dev/greenplum \
   bash
 ```
--- docker_greenplum/rootfs/opt/gpdb/entrypoint.sh
+++ docker_greenplum/rootfs/opt/gpdb/entrypoint.sh
@@ -5,13 +5,14 @@ function gp_init() {
   export DATA_DIRECTORY="${GPDATA}/primary"
   mkdir -pv "${COORDINATOR_DIRECTORY}" "${DATA_DIRECTORY}"
 
-  gpinitsystem -a -c /opt/gpdb/conf/gpinitsystem.conf
+  gpinitsystem -a -c /opt/gpdb/conf/gpinitsystem.conf \
+  && echo "GPDB has successfully init and now started!"
 
-  export COORDINATOR_DATA_DIRECTORY="${COORDINATOR_DIRECTORY}/gpseg-1"
-  echo 'host  all  all  0.0.0.0/0  password' >>"${COORDINATOR_DATA_DIRECTORY}/pg_hba.conf"
-
-  gpconfig -c log_statement -v none
-  gpconfig -c gp_enable_global_deadlock_detector -v on
+  export USER=$(whoami) \
+  && export COORDINATOR_DATA_DIRECTORY="${COORDINATOR_DIRECTORY}/gpseg-1" \
+  && echo 'host  all  all  0.0.0.0/0  password' >> "${COORDINATOR_DATA_DIRECTORY}/pg_hba.conf" \
+  && gpconfig -c log_statement -v none \
+  && gpconfig -c gp_enable_global_deadlock_detector -v on
 
   sleep 5s
 }
