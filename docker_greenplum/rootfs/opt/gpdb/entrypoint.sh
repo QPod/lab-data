4bd6e850c683660cfd85cd82fca694eb7699110c
Bibo Hao <haobibo@users.noreply.github.com>
2023-04-19 02:43:06 +0800

    update init function

--- docker_greenplum/rootfs/opt/gpdb/entrypoint.sh
+++ docker_greenplum/rootfs/opt/gpdb/entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-/var/run/sshd/sshd_start.sh && sleep 5s
+function gp_init() {
+  export COORDINATOR_DIRECTORY="${GPDATA}/coordinator"
+  export DATA_DIRECTORY="${GPDATA}/primary"
+  mkdir -pv "${COORDINATOR_DIRECTORY}" "${DATA_DIRECTORY}"
 
-if [ "`ls -A $GPDB_DATA`" = "" ]; then
-    export COORDINATOR_DIRECTORY=$GPDB_DATA/coordinator
+  gpinitsystem -a -c /opt/gpdb/conf/gpinitsystem.conf
 
-    gpinitsystem -a --ignore-warnings -c /home/gpadmin/gpinitsystem_config_singlenode -h /home/gpadmin/gp_hosts_list
-    psql -d postgres -U gpadmin -f /home/gpadmin/initdb_gpdb.sql
-    gpconfig -c log_statement -v none
-    gpconfig -c gp_enable_global_deadlock_detector -v on
-    echo 'host  all  all  0.0.0.0/0  password' >> ${COORDINATOR_DIRECTORY}/gpseg-1/pg_hba.conf
-    sleep 5s && gpstop -u && tail -f gpAdminLogs/*.log
+  export COORDINATOR_DATA_DIRECTORY="${COORDINATOR_DIRECTORY}/gpseg-1"
+  echo 'host  all  all  0.0.0.0/0  password' >>"${COORDINATOR_DATA_DIRECTORY}/pg_hba.conf"
+
+  gpconfig -c log_statement -v none
+  gpconfig -c gp_enable_global_deadlock_detector -v on
+
+  sleep 5s
+}
+
+sudo /usr/sbin/sshd -D && sleep 5s
+
+if [ "$(ls -A ${GPDATA})" = "" ]; then
+  gp_init && gpstop -u && tail -f gpAdminLogs/*.log
 else
-    gpstart -a && tail -f gpAdminLogs/*.log
+  gpstart -a && tail -f gpAdminLogs/*.log
 fi
