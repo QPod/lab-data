5b8deb7e09bbd9cfc878ef920b69f8074e655405
Bibo Hao <haobibo@users.noreply.github.com>
2024-04-11 11:59:49 +0000

    debug 2

--- docker_postgres/postgres-ext.Dockerfile
+++ docker_postgres/postgres-ext.Dockerfile
@@ -8,12 +8,13 @@ LABEL maintainer="haobibo@gmail.com"
 
 COPY rootfs /
 
-RUN set -x && . /opt/utils/script-utils.sh && . /opt/utils/script-setup-pg_ext.sh \
+RUN set -x && . /opt/utils/script-utils.sh && . /opt/utils/script-setup-pg_ext_mirror.sh \
  && apt-get update && apt-get install -y gettext \
  && envsubst < /opt/utils/install_list_pgext.tpl.apt > /opt/utils/install_list_pgext.apt \
  && rm -rf /opt/utils/install_list_pgext.tpl.apt \
  && echo "To install PG extensions: $(cat /opt/utils/install_list_pgext.apt)" \
  && install_apt /opt/utils/install_list_pgext.apt \
+ && . /opt/utils/script-setup-pg_ext.sh \
  && ls -alh /usr/share/postgresql/*/extension/*.control | sort \
  && echo "Clean up" && list_installed_packages && install__clean
 
--- docker_postgres/rootfs/opt/utils/script-setup-pg_ext.sh
+++ docker_postgres/rootfs/opt/utils/script-setup-pg_ext.sh
@@ -1,22 +1,11 @@
 export USE_PGXS=1
 
-export DISTRO_NAME=$(awk '{ print tolower($0) }' <<< $(lsb_release -is))
-export DISTRO_CODE_NAME=$(lsb_release -cs)
-
-# apt source for: https://packagecloud.io/citusdata/community
-curl -fsSL "https://packagecloud.io/citusdata/community/gpgkey" | gpg --dearmor > /etc/apt/trusted.gpg.d/citusdata_community.gpg
-echo "deb https://packagecloud.io/citusdata/community/${DISTRO_NAME}/ ${DISTRO_CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/citusdata_community.list
-
-# apt source for: https://packagecloud.io/timescale/timescaledb
-curl -fsSL "https://packagecloud.io/timescale/timescaledb/gpgkey" | gpg --dearmor > /etc/apt/trusted.gpg.d/timescale_timescaledb.gpg
-echo "deb https://packagecloud.io/timescale/timescaledb/${DISTRO_NAME}/ ${DISTRO_CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/timescale_timescaledb.list
-
-# apt source for: https://packagecloud.io/pigsty/pgsql
-curl -fsSL "https://packagecloud.io/pigsty/pgsql/gpgkey" | gpg --dearmor > /etc/apt/trusted.gpg.d/pigsty_pgsql.gpg
-echo "deb https://packagecloud.io/pigsty/pgsql/${DISTRO_NAME}/ ${DISTRO_CODE_NAME} main" | sudo tee /etc/apt/sources.list.d/pigsty_pgsql.list
+setup_apache_age() {
+    cd /tmp
+    git clone --depth 1 -b PG15 https://github.com/apache/age
+    cd /tmp/age
+    make -j8 && make install
+}
 
 
-cd /tmp
-git clone --depth 1 -b PG15 https://github.com/apache/age
-cd /tmp/age
-make && make install
+setup_apache_age
