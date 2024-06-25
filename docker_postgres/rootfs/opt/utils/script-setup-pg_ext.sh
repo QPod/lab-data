3fdb8cfbf0019464bd6a330041dfdc940b52a925
Bibo Hao <haobibo@users.noreply.github.com>
2024-06-26 03:18:04 +0800

    typo2

--- docker_postgres/rootfs/opt/utils/script-setup-pg_ext.sh
+++ docker_postgres/rootfs/opt/utils/script-setup-pg_ext.sh
@@ -32,7 +32,8 @@ setup_pgvectorscale() {
     ARCH="amd64" \
  && VER_PGVS=$(curl -sL https://github.com/timescale/pgvectorscale/releases.atom | grep 'releases/tag' | head -1 | grep -Po '\d[\d.]+' ) \
  && URL_PGVS="https://github.com/timescale/pgvectorscale/releases/download/${VER_PGVS}/pgvectorscale-${VER_PGVS}-pg${PG_MAJOR}-${ARCH}.zip" \
- && install_zip ${URL_PGVS} && mv /opt/pgvectorscal* /tmp/pgvectorscale && cd /tmp/pgvectorscale \
+ && mkdir -pv /tmp/pgvectorscale/ && cd /tmp/pgvectorscale \
+ && install_zip ${URL_PGVS} && mv /opt/pgvectorscal* /tmp/pgvectorscale/ 
  && dpkg -i *.deb
 }
 setup_pgvectorscale
